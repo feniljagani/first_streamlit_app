8e4abf61b2a2cf998020939d6f3dbf50714cb071
feniljagani <84614757+feniljagani@users.noreply.github.com>
2022-05-11 16:26:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,16 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+##New section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json()) # just writes the data to screen
+
+# take the json version of the response and normalize it
+fruityvice_normalized = pamdas.json_noemalize(fruityvice_response.json())
+
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
